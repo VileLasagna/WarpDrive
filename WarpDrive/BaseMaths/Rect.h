2647d0e1ed9287de1951c92e1f8a7f8a99d4c59f
Vile Lasagna <vilelasagna@gmail.com>
2015-03-07 14:40:24 -0300

    Changed Rect so that it's a constexpr type

--- WarpDrive/BaseMaths/Rect.h
+++ WarpDrive/BaseMaths/Rect.h
@@ -7,37 +7,37 @@ template <class T>
 class Rect
 {
 public:
-    Rect() : mX(0), MX(0), mY(0), MY(0) {}
+	constexpr Rect() : mX(0), MX(0), mY(0), MY(0) {}
 
-    Rect(T minX, T maxX, T minY, T maxY) : 
-        mX(minX), MX(maxX), mY(minY), MY(maxY) {}
+	constexpr Rect(T minX, T maxX, T minY, T maxY) :
+			mX(minX), MX(maxX), mY(minY), MY(maxY) {}
 
-    T MinX() const { return mX; }
-    T MaxX() const { return MX; }
-    T MinY() const { return mY; }
-    T MaxY() const { return MY; }
+	constexpr T MinX() const { return mX; }
+	constexpr T MaxX() const { return MX; }
+	constexpr T MinY() const { return mY; }
+	constexpr T MaxY() const { return MY; }
 
-    T Width() const { return MX - mX; }
-    T Height() const { return MY - mY; }
+	constexpr T Width() const  { return MX - mX; }
+	constexpr T Height() const { return MY - mY; }
 
-	void setColour (SDLRGBAColour& c);
+	constexpr void setColour (const SDLRGBAColour& c) {colour = c;}
 
-	bool intersects(const Rect<T>& r) const
+	constexpr bool intersects(const Rect<T>& rhs) const
 	{
-    return ( 
-          MinX() <= r.MaxX() &&
-        r.MinX() <=   MaxX() &&
-          MinY() <= r.MaxY() &&
-        r.MinY() <=   MaxY() );
+		return ( MinX()     <= rhs.MaxX() &&
+			    rhs.MinX() <=   MaxX()   &&
+			    MinY()     <= rhs.MaxY() &&
+			    rhs.MinY() <=   MaxY()     );
+
 
 	}
 
 private:
-    T mX, MX, mY, MY;
+	T mX, MX, mY, MY;
 	SDLRGBAColour colour;
 };
 
-typedef Rect<float> Rectf;
-typedef Rect<int> Recti;
+using Rectf = Rect<float>;
+using Recti = Rect<int>;
 
 #endif
