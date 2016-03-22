dc2335737bf3acff76ff953c33ce3180ba204071
Vile Lasagna <vilelasagna@gmail.com>
2016-03-21 22:00:30 -0300

    Added an util header and source.
    Used to put constants and helper functions. Currently has a floating
    point comparison function and precision threshold

---
+++ WarpDrive/basesystem/util.cpp
@@ -0,0 +1,13 @@
+#include "basesystem/util.hpp"
+
+#include <cmath>
+
+namespace WrpDrv
+{
+
+    bool flEquals(double f1, double f2)
+    {
+        return( (fabs(f1) - fabs(f2)) <= WrpDrv::flEpsilon);
+    }
+
+}
---
+++ WarpDrive/basesystem/util.hpp
@@ -0,0 +1,14 @@
+#ifndef WD_UTIL_HPP_DEFINED
+#define WD_UTIL_HPP_DEFINED
+
+
+namespace WrpDrv
+{
+    constexpr double flEpsilon = 0.00001;
+
+    bool flEquals(double f1, double f2);
+}
+
+
+
+#endif // WD_UTIL_HPP_DEFINED
